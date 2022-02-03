c9d3ba7469f6c74e613993557a853c8adbb58b7e
bahul <bahulbert03@gmail.com>
2022-02-03 09:47:09 -0500

    addedfunc

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,15 @@
 #include <Arduino.h>
+int addtwoint(int, int);
 
 void setup() {
   // put your setup code here, to run once:
+  addtwoint(5, 6);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+}
+
+int addtwoint(int a, int b) {
+  return a+b;
 }
