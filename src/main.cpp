838ec8b73593d772b8f1759389eadbc58a36a5d2
bahul <bahulbert03@gmail.com>
2022-02-03 09:52:02 -0500

    firstcommitnewbranch

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,15 @@
 #include <Arduino.h>
+int mult2int(int, int);
 
 void setup() {
   // put your setup code here, to run once:
+  mult2int(5, 7);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+}
+
+int mult2int(int a, int b) {
+  return a*b;
 }
